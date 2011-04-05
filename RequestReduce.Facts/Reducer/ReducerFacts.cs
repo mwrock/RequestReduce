3608b9fc023ba1ae83625bfb58183999dd85f522
Matt Wrock <matt@mattwrock.com>
2011-04-04 23:22:16 -0700

    added a few reducer tests

--- RequestReduce.Facts/Reducer/ReducerFacts.cs
+++ RequestReduce.Facts/Reducer/ReducerFacts.cs
@@ -1,4 +1,8 @@
-﻿using RequestReduce.Reducer;
+﻿using System;
+using Moq;
+using RequestReduce.Configuration;
+using RequestReduce.Reducer;
+using RequestReduce.Utilities;
 using Xunit;
 
 namespace RequestReduce.Facts.Reducer
@@ -16,14 +20,50 @@ namespace RequestReduce.Facts.Reducer
         public class Process
         {
             [Fact]
-            public void WillReturnProcessedCssUrl()
+            public void WillReturnProcessedCssUrlInCorrectConfigDirectory()
+            {
+                var testable = new TestableReducer();
+                testable.Mock<IConfigurationWrapper>().Setup(x => x.SpriteDirectory).Returns("spritedir");
+
+                var result = testable.ClassUnderTest.Process("http://host/css1.css::http://host/css2.css");
+
+                Assert.True(result.StartsWith("spritedir/"));
+            }
+
+            [Fact]
+            public void WillReturnProcessedCssUrlWithAGuidName()
+            {
+                var testable = new TestableReducer();
+                testable.Mock<IConfigurationWrapper>().Setup(x => x.SpriteDirectory).Returns("spritedir");
+                Guid guid;
+
+                var result = testable.ClassUnderTest.Process("http://host/css1.css::http://host/css2.css");
+
+                Assert.True(Guid.TryParse(result.Substring("spritedir/".Length, result.Length - "spritedir/".Length - ".css".Length), out guid));
+            }
+
+            [Fact]
+            public void WillReturnProcessedCssUrlWithAcssExtension()
             {
                 var testable = new TestableReducer();
 
                 var result = testable.ClassUnderTest.Process("http://host/css1.css::http://host/css2.css");
 
-                Assert.Equal("http://host/processed.css", result);
+                Assert.True(result.EndsWith(".css"));
             }
+
+            [Fact]
+            public void WillDownloadContentOfEachOriginalCSS()
+            {
+                var testable = new TestableReducer();
+
+                var result = testable.ClassUnderTest.Process("http://host/css1.css::http://host/css2.css");
+
+                testable.Mock<IWebClientWrapper>().Verify(x => x.DownloadString("http://host/css1.css"), Times.Once());
+                testable.Mock<IWebClientWrapper>().Verify(x => x.DownloadString("http://host/css2.css"), Times.Once());
+            }
+
+
         }
     }
 }
