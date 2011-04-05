c12a6c67b7c958f5827f18491600c842265d2261
Matt Wrock <matt@mattwrock.com>
2011-04-05 06:17:42 -0700

    added a test for minifying, aggregating and saving css

--- RequestReduce.Facts/Reducer/ReducerFacts.cs
+++ RequestReduce.Facts/Reducer/ReducerFacts.cs
@@ -63,6 +63,18 @@ namespace RequestReduce.Facts.Reducer
                 testable.Mock<IWebClientWrapper>().Verify(x => x.DownloadString("http://host/css2.css"), Times.Once());
             }
 
+            [Fact]
+            public void WillSaveMinifiedAggregatedCSS()
+            {
+                var testable = new TestableReducer();
+                testable.Mock<IWebClientWrapper>().Setup(x => x.DownloadString("http://host/css1.css")).Returns("css1");
+                testable.Mock<IWebClientWrapper>().Setup(x => x.DownloadString("http://host/css2.css")).Returns("css2");
+                testable.Mock<IMinifier>().Setup(x => x.Minify("css1css2")).Returns("min");
+
+                var result = testable.ClassUnderTest.Process("http://host/css1.css::http://host/css2.css");
+
+                testable.Mock<IFileWrapper>().Verify(x => x.Save("min", It.IsAny<string>()), Times.Once());
+            }
 
         }
     }
