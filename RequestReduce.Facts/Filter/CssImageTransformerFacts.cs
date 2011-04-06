a1d0a173e1597407e4028381e918c48cd027b70f
Matt Wrock <matt@mattwrock.com>
2011-04-06 09:40:50 -0700

    started cssImageTransformer

---
+++ RequestReduce.Facts/Filter/CssImageTransformerFacts.cs
@@ -0,0 +1,57 @@
+﻿using RequestReduce.Reducer;
+using System.Linq;
+using Xunit;
+
+namespace RequestReduce.Facts.Filter
+{
+    public class CssImageTransformerFacts
+    {
+        class TestableCssImageTransformer : Testable<CssImageTransformer>
+        {
+            public TestableCssImageTransformer()
+            {
+                
+            }
+        }
+
+        public class ExtractImageUrls
+        {
+            [Fact]
+            public void WillReturnBackgroundImages()
+            {
+                var testable = new TestableCssImageTransformer();
+                var css =
+                    @"
+.LocalNavigation {    
+    background-image: url('http://i1.social.microsoft.com/contentservice/798d3f43-7d1e-41a1-9b09-9dad00d8a996/subnav_technet.png');
+}
+
+.LocalNavigation .TabOn,.LocalNavigation .TabOn:hover {
+    background-image: url(""http://i3.social.microsoft.com/contentservice/1f22465a-498c-46f1-83d3-9dad00d8a950/subnav_on_technet.png"");
+}";
+
+                var result = testable.ClassUnderTest.ExtractImageUrls(css);
+
+                Assert.Equal(2, result.Count());
+                Assert.True(result.Contains("http://i1.social.microsoft.com/contentservice/798d3f43-7d1e-41a1-9b09-9dad00d8a996/subnav_technet.png"));
+                Assert.True(result.Contains("http://i3.social.microsoft.com/contentservice/1f22465a-498c-46f1-83d3-9dad00d8a950/subnav_on_technet.png"));
+            }
+
+            [Fact]
+            public void WillNotReturnReapeatingBackgroundImages()
+            {
+                var testable = new TestableCssImageTransformer();
+                var css =
+                    @"
+.LocalNavigation .TabOn,.LocalNavigation .TabOn:hover {
+    background: url(""http://i3.social.microsoft.com/contentservice/1f22465a-498c-46f1-83d3-9dad00d8a950/subnav_on_technet.png"") repeat;
+}";
+
+                var result = testable.ClassUnderTest.ExtractImageUrls(css);
+
+                Assert.Equal(0, result.Count());
+            }
+
+        }
+    }
+}
--- RequestReduce/Reducer/CssImageTransformer.cs
+++ RequestReduce/Reducer/CssImageTransformer.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace RequestReduce.Reducer
 {
     public class CssImageTransformer : ICssImageTransformer
     {
+        private static readonly Regex classPattern = new Regex("\\{[^\\}]+\\}", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        private static readonly Regex imageUrlPattern = new Regex(@"background(-image)?:[\s\w]*url[\s]*\([\s]*(?<url>[^\)]*)[\s]*\)(?:(?! repeat|(-[0-9])|( 0px))[^;])*;", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
         public IEnumerable<string> ExtractImageUrls(string cssContent)
         {
-            throw new NotImplementedException();
+            var urls = new List<string>();
+            foreach (var classMatch in classPattern.Matches(cssContent))
+            {
+                var urlMatch = imageUrlPattern.Match(classMatch.ToString());
+                if(urlMatch.Success)
+                    urls.Add(urlMatch.Groups["url"].ToString().Replace("'", "").Replace("\"",""));
+            }
+            return urls;
         }
 
         public string InjectSprite(string imageUrl, Sprite sprite)
