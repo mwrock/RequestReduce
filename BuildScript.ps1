b9088c36be4ff6b4040861aabc2cc0e6fa2ac34a
Matt Wrock <matt@mattwrock.com>
2011-05-23 22:06:46 -0700

    automate iis setup

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -1,6 +1,7 @@
 $psake.use_exit_on_error = $true
 properties {
     $baseDir = resolve-path .
+	$port = "8888"
     $configuration = "debug"
 	# Package Directories
 	$filesDir = "$baseDir\BuildFiles"
@@ -12,7 +13,11 @@ properties {
 	}
 }
 
-task Default -depends Clean-Solution, Build-Solution, Test-Solution, Build-Nuget
+task Default -depends Clean-Solution, Setup-IIS, Build-Solution, Test-Solution, Build-Nuget
+
+task Setup-IIS {
+    Setup-IIS "RequestReduce" $baseDir $port
+}
 
 task Clean-Solution -depends Clean-BuildFiles {
     exec { msbuild RequestReduce.sln /t:Clean /v:quiet }
@@ -55,4 +60,48 @@ function create([string[]]$paths) {
             new-item -path $path -type directory | out-null
         }
     }
+}
+
+function Load-IISProvider {
+    $module = Get-Module | Where-Object {$_.Name -eq "WebAdministration"}
+    if($module -eq $null) {
+        Import-Module WebAdministration
+    }
+}
+
+function Setup-IIS([string] $siteName, [string] $solutionDir, [string] $port )
+{
+  try
+  {
+    Load-IISProvider
+
+    # cleanup
+	echo "looking for $siteName website"
+    if(Test-Path IIS:\Sites\$siteName)
+    {
+		return
+	}
+
+    echo "Setting up $siteName website"
+    $websitePhysicalPath = $solutionDir + "\RequestReduce.SampleWeb"
+     
+    # Create the site
+    $id = (Get-ChildItem IIS:\Sites | foreach {$_.id} | sort -Descending | select -first 1) + 1
+    New-Website -Name $siteName -Port $port -PhysicalPath $websitePhysicalPath -Id $id
+       
+    # Create app pool and have it run under network service
+    $appPool = New-Item -Force IIS:\AppPools\$siteName
+    $appPool.processModel.identityType = "NetworkService"
+    $appPool.managedRuntimeVersion = "v4.0.30319"
+    $appPool | Set-Item
+        
+    # Set app pool
+    Set-ItemProperty IIS:\Sites\$siteName -name applicationPool -value $siteName
+
+    #Start Site
+    Start-WebItem IIS:\Sites\$siteName
+ }
+ catch {
+   "Error in Setup-IIS: " + $_.Exception
+ }
 }
