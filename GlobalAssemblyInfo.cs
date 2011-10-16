89d73e0413136d7c27e7a4a87be16e2719375af7
Matt Wrock <matt@mattwrock.com>
2011-10-15 21:34:45 -0700

    deploying 1.2.9

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("RequestReduce")]
 [assembly: AssemblyCopyright("Copyright Matt Wrock 2011")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersion("1.2.7")]
-[assembly: AssemblyFileVersion("1.2.7")]
+[assembly: AssemblyVersion("1.2.9")]
+[assembly: AssemblyFileVersion("1.2.9")]
