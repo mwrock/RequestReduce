0f55930dbc4c66b3542dc357d5522563074b42d7
Matt Wrock <matt@mattwrock.com>
2011-10-24 03:16:46 -0700

    deploying 1.3.3

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.15")]
-[assembly: AssemblyFileVersion("1.2.15")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
