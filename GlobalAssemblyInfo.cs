b246db3c4b139f3db55ac8be06e44f8e5a5e9eab
Matt Wrock <matt@mattwrock.com>
2011-11-04 08:48:42 -0700

    deploying 1.3.20

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.3")]
-[assembly: AssemblyFileVersion("1.3.3")]
-[assembly: AssemblyTrademarkAttribute("0000000000000000000000000000000000000000")]
+[assembly: AssemblyVersion("1.3.20")]
+[assembly: AssemblyFileVersion("1.3.20")]
+[assembly: AssemblyTrademarkAttribute("41180f9d44c3b2cfe4b902dcbb80e1ce01e14bb4")]
