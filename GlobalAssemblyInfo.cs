10848f95ada0ee1c9e9d5b9f9c988f7bad327564
Matt Wrock <matt@mattwrock.com>
2011-11-04 09:33:15 -0700

    fix releasenoptes

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.4")]
-[assembly: AssemblyFileVersion("1.4.4")]
-[assembly: AssemblyTrademarkAttribute("2c3af2066b2557c525fe32788fb0e4f43d3766ab")]
+[assembly: AssemblyVersion("1.4.5")]
+[assembly: AssemblyFileVersion("1.4.5")]
+[assembly: AssemblyTrademarkAttribute("ba1f37a277e156b6f923316e70283c98d0a705dc")]
