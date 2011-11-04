d5110afa3fd4ded827ad32ef10ce3904584162ce
Matt Wrock <matt@mattwrock.com>
2011-11-04 09:45:06 -0700

    deploying 1.4.8

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.6")]
-[assembly: AssemblyFileVersion("1.4.6")]
-[assembly: AssemblyTrademarkAttribute("10848f95ada0ee1c9e9d5b9f9c988f7bad327564")]
+[assembly: AssemblyVersion("1.4.8")]
+[assembly: AssemblyFileVersion("1.4.8")]
+[assembly: AssemblyTrademarkAttribute("cc8f8198c4746b2faf08e429026d3b5d7301205f")]
