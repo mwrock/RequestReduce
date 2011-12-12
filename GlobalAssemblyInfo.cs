eade613cd0ba32b4778a3bfcbc849fad8be07c56
Matt Wrock <matt@mattwrock.com>
2011-12-12 09:30:36 -0800

    deploying 1.6.9

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyVersion("1.6.9")]
+[assembly: AssemblyFileVersion("1.6.9")]
 [assembly: AssemblyTrademarkAttribute("")]
