241d1d90dc5fecb4b475ddfb812d1b22b066acac
Matt Wrock <matt@mattwrock.com>
2011-12-22 20:43:23 -0800

    starting css class flattening

---
+++ RequestReduce.Facts/Reducer/CssSelectorAnalyzerFacts.cs
@@ -0,0 +1,128 @@
+﻿using RequestReduce.Reducer;
+using Xunit;
+
+namespace RequestReduce.Facts.Reducer
+{
+    public class CssSelectorAnalyzerFacts
+    {
+        [Fact]
+        public void WillMatchOnSingleElements()
+        {
+            var target = "#icons .new .warnings.red div";
+            var comparable = "div";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WillMatchOnMultipleElements()
+        {
+            var target = "#icons .new .warnings.red div";
+            var comparable = ".new div";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WillNotMatchOnMultipleElementsInDifferentOrder()
+        {
+            var target = "#icons .new .warnings.red div";
+            var comparable = "div .new";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void WillNotMatcWithForeignElement()
+        {
+            var target = "#icons .new .warnings.red div";
+            var comparable = ".new div h1";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void WillMatcMatchingElementWithoutClassToElementWithClass()
+        {
+            var target = "#icons .new .warnings.red div.cls";
+            var comparable = ".new div";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WillNotMatchPartialStringMatch()
+        {
+            var target = "#icons .new .warnings.red div.cls";
+            var comparable = ".ne div";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void WillMatchelementWithClassToSameElementWithSameClass()
+        {
+            var target = "#icons .new .warnings.red div.cls";
+            var comparable = "#icons div.cls";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WillNotMatcMatchingElementWithClassToElementWithOutClass()
+        {
+            var target = "#icons .new .warnings.red div";
+            var comparable = ".new div.cls";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void WillMatcSingleClassToMiltiClassContainingSingleClass()
+        {
+            var target = "#icons .new .warnings.red div.cls";
+            var comparable = "#icons .red";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void WillMatcElementAfterPartialMatch()
+        {
+            var target = "#icons .new .ne .warnings.red.small div.cls";
+            var comparable = "#icons .ne";
+            var testable = new CssSelectorAnalyzer();
+
+            var result = testable.IsInScopeOfTarget(target, comparable);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ RequestReduce/Reducer/CssSelectorAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RequestReduce.Reducer
+{
+    public class CssSelectorAnalyzer
+    {
+        public bool IsInScopeOfTarget(string targetSelector, string comparableSelector)
+        {
+            var targetOffset = 0;
+            var tokens = comparableSelector.Split(new char[] {}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var token in tokens)
+            {
+                targetOffset = FindToken(token, targetSelector, targetOffset);
+                if(targetOffset == -1)
+                    return false;
+            }
+            return true;
+        }
+
+        private int FindToken(string comparableSelector, string targetSelector, int targetOffset)
+        {
+            while (targetSelector.Length > targetOffset)
+            {
+                var idx = targetSelector.IndexOf(comparableSelector, targetOffset, StringComparison.OrdinalIgnoreCase);
+                if (idx == -1) return idx;
+                var endIdx = idx + comparableSelector.Length;
+                if (targetSelector.Length <= endIdx || targetSelector[endIdx] == '.' || targetSelector[endIdx] == '#' ||
+                    targetSelector.IndexOfAny(new[] {' ', '\n', '\r', '\t'}, endIdx, 1) == endIdx)
+                    return idx;
+                targetOffset = endIdx;
+            }
+            return -1;
+        }
+    }
+}
