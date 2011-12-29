06f4ea77e0de34b189d961f158ad42943f5e4cfc
Matt Wrock <matt@mattwrock.com>
2011-12-28 21:21:32 -0800

    fix nuget dependencies and install/uninstall for web site projects

--- RequestReduce/Nuget/Tools/install.ps1
+++ RequestReduce/Nuget/Tools/install.ps1
@@ -2,11 +2,14 @@
 
 . (Join-Path $toolsPath "AddPostBuildScript.ps1")
 
+if($project.Object -is [VsWebSite.VSWebSite]) {
+	$target = Join-Path $project.FullName "bin\\optipng.exe"
+	Remove-Item $target
+}
+else {
 # Get the current Post Build Event cmd
 $currentPostBuildCmd = $project.Properties.Item("PostBuildEvent").Value
 
-# Append our post build command if it's not already there
-if (!$currentPostBuildCmd.Contains($PostBuildScript)) {
-    $project.Properties.Item("PostBuildEvent").Value += $PostBuildScript
+# Remove our post build command from it (if it's there)
+$project.Properties.Item("PostBuildEvent").Value = $currentPostBuildCmd.Replace($PostBuildScript, "")
 }
-
--- RequestReduce/Nuget/Tools/uninstall.ps1
+++ RequestReduce/Nuget/Tools/uninstall.ps1
@@ -2,9 +2,14 @@
 
 . (Join-Path $toolsPath "AddPostBuildScript.ps1")
 
+if($project.Object -is [VsWebSite.VSWebSite]) {
+	$target = Join-Path $project.FullName "bin\\optipng.exe"
+	Remove-Item $target
+}
+else {
 # Get the current Post Build Event cmd
 $currentPostBuildCmd = $project.Properties.Item("PostBuildEvent").Value
 
 # Remove our post build command from it (if it's there)
 $project.Properties.Item("PostBuildEvent").Value = $currentPostBuildCmd.Replace($PostBuildScript, "")
-
+}
