c9405b99e385178dc7be6f04bfd9e3007fed6ae6
Matt Wrock <matt@mattwrock.com>
2012-01-12 02:03:01 -0800

    deploying 1.7.5

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.7.0")]
-[assembly: AssemblyFileVersion("1.7.0")]
-[assembly: AssemblyTrademarkAttribute("")]
+[assembly: AssemblyVersion("1.7.5")]
+[assembly: AssemblyFileVersion("1.7.5")]
+[assembly: AssemblyTrademarkAttribute("b84e214edf41e1b8825dd416a7cba61934e560c1")]
