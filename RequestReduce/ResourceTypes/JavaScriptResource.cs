4b0479862ee21089ca2934fd312b59bec6ecf59d
Matt Wrock <matt@mattwrock.com>
2012-02-05 04:46:10 -0800

    closes #126 will ignore embedded scripts in inline scripts

--- RequestReduce/ResourceTypes/JavaScriptResource.cs
+++ RequestReduce/ResourceTypes/JavaScriptResource.cs
@@ -10,8 +10,8 @@ namespace RequestReduce.ResourceTypes
     public class JavaScriptResource : IResourceType
     {
         private const string ScriptFormat = @"<script src=""{0}"" type=""text/javascript"" ></script>";
-        private readonly Regex scriptPattern = new Regex(@"<script[^>]+>([^<]*</script>)?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly Regex ScriptFilterPattern = new Regex(@"<script[^>]+src=[^>]+>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private readonly Regex scriptPattern = new Regex(@"<script(.*?)(/>|</script>)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        private static readonly Regex ScriptFilterPattern = new Regex(@"^<script[^>]+src=[^>]+>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
         private Func<string, string, bool> tagValidator = ((tag, url) => 
                 {
                     var match = ScriptFilterPattern.Match(tag);
