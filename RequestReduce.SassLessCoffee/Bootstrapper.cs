23f3cc303b9c8023931d7d3c8c19f096dd0e5e7f
Matt Wrock <matt@mattwrock.com>
2012-02-13 06:23:58 -0800

    dont require SassAndCoffee refs if only Less is needed

--- RequestReduce.SassLessCoffee/Bootstrapper.cs
+++ RequestReduce.SassLessCoffee/Bootstrapper.cs
@@ -1,12 +1,29 @@
-﻿using RequestReduce.Api;
+﻿using System;
+using RequestReduce.Api;
 using RequestReduce.Utilities;
 
 namespace RequestReduce.SassLessCoffee
 {
     public static class Bootstrapper
     {
-        public static CoffeeHandler CoffeeHandler = new CoffeeHandler();
-        public static SassHandler SassHandler = new SassHandler();
+        public static CoffeeHandler CoffeeHandler;
+        public static SassHandler SassHandler;
+
+        static Bootstrapper()
+        {
+            try
+            {
+                CoffeeHandler = new CoffeeHandler();
+                SassHandler = new SassHandler();
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format("There were errors Loading Sass and Coffee Handlers");
+                var wrappedException = new ApplicationException(message, ex);
+                if (Registry.CaptureErrorAction != null)
+                    Registry.CaptureErrorAction(wrappedException);
+            }
+        }
 
         public static void Start()
         {
