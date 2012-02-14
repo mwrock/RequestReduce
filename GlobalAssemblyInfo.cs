227a7e1ec392b2710128f259bb21b8083edc4f49
Matt Wrock <matt@mattwrock.com>
2012-02-14 06:34:11 -0800

    deploying 1.7.103

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.7.100")]
-[assembly: AssemblyFileVersion("1.7.100")]
-[assembly: AssemblyTrademarkAttribute("19896071280378fce30b910b13f6a0152bcf1c58")]
+[assembly: AssemblyVersion("1.7.103")]
+[assembly: AssemblyFileVersion("1.7.103")]
+[assembly: AssemblyTrademarkAttribute("c09dcd6c24055c3382325c1dd548a57f8ba06c9a")]
