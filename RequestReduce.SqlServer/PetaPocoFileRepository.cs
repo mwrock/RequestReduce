033a5f1cc65e56f66da62a45514387c753eaaa3d
Mads Storm <madsstorm@gmail.com>
2012-02-17 19:57:36 +0100

    PetaPoco FileRepository

---
+++ RequestReduce.SqlServer/PetaPocoFileRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RequestReduce.SqlServer
+{
+    public interface IPetaPocoFileRepository : IPetaPocoRepository
+    {
+        IEnumerable<string> GetActiveFiles();
+        IEnumerable<RequestReduceFile> GetFilesFromKey(Guid key);
+        string GetActiveUrlByKey(Guid key, Type resourceType);
+    }
+
+    public class PetaPocoFileRepository : PetaPocoRepository, IPetaPocoFileRepository
+    {
+        public IEnumerable<string> GetActiveFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RequestReduceFile> GetFilesFromKey(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetActiveUrlByKey(Guid key, Type resourceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
