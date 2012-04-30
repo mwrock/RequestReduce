5f5f8fae9b14284ff84a50013dc0baa072f6e4c4
Matt Wrock <matt@mattwrock.com>
2012-04-30 02:36:10 -0700

    deploying 1.8.32

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.8.30")]
-[assembly: AssemblyFileVersion("1.8.30")]
-[assembly: AssemblyTrademarkAttribute("ff6fe7af6e9f02b30025bcc265bf4e60acd6cc8b")]
+[assembly: AssemblyVersion("1.8.32")]
+[assembly: AssemblyFileVersion("1.8.32")]
+[assembly: AssemblyTrademarkAttribute("3c1e28dc00dbae96d4019fd9a88d7450a1bd550e")]
